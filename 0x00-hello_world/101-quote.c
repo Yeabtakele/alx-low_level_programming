255ab9738eedf00809b4eb39a2c48bf79436e559
yeabtakele <bereketabe434@gmail.com>
2023-09-07 15:17:33 -0700

    <_

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,14 @@
-#!/bin/bash
-gcc -S -masm=intel $CFILE
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - prints exactly "and that piece of art is useful"
+ * - athor yeabbereket, 2023-8-9",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - yeabtakele, 2023-9-8\n", 59);
+	return (1);
+}
