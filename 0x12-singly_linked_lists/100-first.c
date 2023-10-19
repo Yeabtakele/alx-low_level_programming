6bf6e15998932b017cd09ad3d7d5b35dd0c4e3ac
yeabtakele <bereketabe434@gmail.com>
2023-10-19 15:51:47 -0700

    Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void __attribute__((constructor)) hare(void);
  */
 void hare(void)
 {
-	printf("You're best!work hard ,\n"
-	       "you well buy  house upon you dearm!\n");
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
